2d9fc55a163191ae7035332fb9a6f33f4aee03fe
Vova <vova.banudz@gmail.com>
2024-01-04 22:57:44 +0200

    add validator

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,54 +11,19 @@ use App\Models\ApiToken;
 use App\Models\Position;
 use App\Models\User;
 use App\Services\ImageCropService;
+use App\Services\Validator;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function getAll(Request $request){
 
-        $page = (int)1;
-        $offset = (int)0;
-        $count = (int)5;
-        if ($request->has('page')){
-            $page = $request->page;
-        }
+        $validator = new Validator($request);
 
-        if ($request->has('offset') && $request->offset > 0){
-            $offset = intval($request->offset);
-        }
-
-        if ($request->has('count')){
-            $count = $request->count;
-        }
-
-        $fails =[];
-
-        if(!preg_match("/^[0-9]+$/", $count)){
-            $fails['count'] =
-                ['The count must be an integer.'];
-        }
-
-        if($page == 0){
-            $fails['page'] =
-                ['The page must be at least 1.'];
-        }
-
-        if(!preg_match("/^[0-9]+$/", $page)){
-            $fails['page'] =
-                ['The page must be an integer.'];
-        }
-
-        if(!empty($fails)){
-
-            $data = [
-                'success' => false,
-                'message' => 'Validation failed',
-                'fails' => $fails
-            ];
-            return response()->json($data, 422);
-        }
-
+        $validator->validateAllUsers();
+        $page = $validator->page;
+        $offset = $validator->offset;
+        $count = $validator->count;
 
         $total_users =  User::query()->count();
         $total_pages =  ceil(($total_users - $offset)/$count);
@@ -78,8 +43,6 @@ class UserController extends Controller
                 "message" => 'Page not found',
             ], 404);
         }
-
-
         $users = UserResourceCollection::collection(User::query()->offset($offsetBd)->limit($count)->get());
 
 
@@ -99,97 +62,18 @@ class UserController extends Controller
 
     public function get($id)
     {
-
-        if(!preg_match("/^[0-9]+$/", $id)){
-            $data = [
-                'success' => false,
-                'message' => 'Validation failed',
-                'fails' => [
-                    'user_id' => [
-                        'The user_id must be an integer.'
-                            ]
-                        ]
-                    ];
-            return response()->json($data, 400);
-        }
-        $user = User::find($id);
-
-        if(!$user){
-            $data = [
-                'success' => false,
-                'message' => 'The user with the requested identifier does not exist',
-                'fails' => [
-                    'user_id' => [
-                        'User not found'
-                    ]
-                ]
-            ];
-            return response()->json($data, 404);
-        }
+        $user = Validator::validateUser($id);
         return new UserResource($user);
     }
 
 
-
-
-
     public function create(UserRequest $request){
 
-//        Validation token
-        $token = $request->header('Token');
-
-        if(!$token){
-            $data = [
-                'success' => false,
-                'message' => 'The token not exist.'
-            ];
-            return response()->json($data, 401);
-        }
-
-        $token = ApiToken::query()->where('token', $token)->first();
-
-        $created = strtotime($token->created_at);
-        $end = $created + (40*60);
-        $now = strtotime(now());
-        $status = $now<$end;
-
-        if(!$status){
-            $data = [
-                'success' => false,
-                'message' => 'The token expired.'
-            ];
-            return response()->json($data, 401);
-        }
-//        Delete token
-        $token->delete();
-//        End of Validation token
-
+        Validator::checkToken($request);
         $data = $request->validated();
-
-        $user = User::query()
-            ->where('email', $data['email'])
-            ->orWhere('phone', $data['phone'])
-            ->first();
-
-        if($user){
-            $data = [
-                'success' => false,
-                'message' => 'User with this phone or email already exist',
-            ];
-            return response()->json($data, 409);
-        }
-
-        if(!Position::query()->find($request->position_id)){
-            $data = [
-                'success' => false,
-                'message' => 'Position with this id not exist',
-            ];
-            return response()->json($data, 404);
-        }
-
+        Validator::validateCreateUser($data);
 
         $cropService = new ImageCropService();
-//        $file = $request->file('photo')->store('images');
         $file =  $cropService->save($request->file('photo'));
         $data['photo'] = $file;
         $user = User::query()->create($data);
@@ -200,9 +84,6 @@ class UserController extends Controller
             'message' => 'New user successfully registered',
         ];
         return response()->json($data, 200);
-
-
-
 
     }
 }
---
+++ app/Services/Validator.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\ApiToken;
+use App\Models\Position;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class Validator
+{
+
+    public int $page = 1;
+    public int $offset = 0;
+    public int $count = 5;
+
+    public function __construct(Request $request)
+    {
+        if ($request->has('page')){
+            $this->page = $request->page;
+        }
+
+        if ($request->has('offset') && $request->offset > 0){
+            $this->offset = intval($request->offset);
+        }
+
+        if ($request->has('count')){
+            $this->count = $request->count;
+        }
+    }
+
+    public function validateAllUsers()
+    {
+        $fails =[];
+
+        if(!preg_match("/^[0-9]+$/", $this->count)){
+            $fails['count'] =
+                ['The count must be an integer.'];
+        }
+
+        if($this->page == 0){
+            $fails['page'] =
+                ['The page must be at least 1.'];
+        }
+
+        if(!preg_match("/^[0-9]+$/", $this->page)){
+            $fails['page'] =
+                ['The page must be an integer.'];
+        }
+
+        if(!empty($fails)){
+
+            $data = [
+                'success' => false,
+                'message' => 'Validation failed',
+                'fails' => $fails
+            ];
+            return response()->json($data, 422);
+        }
+    }
+
+
+    public static function validateUser($id)
+    {
+        if(!preg_match("/^[0-9]+$/", $id)){
+            $data = [
+                'success' => false,
+                'message' => 'Validation failed',
+                'fails' => [
+                    'user_id' => [
+                        'The user_id must be an integer.'
+                    ]
+                ]
+            ];
+            return response()->json($data, 400);
+        }
+        $user = User::find($id);
+
+        if(!$user){
+            $data = [
+                'success' => false,
+                'message' => 'The user with the requested identifier does not exist',
+                'fails' => [
+                    'user_id' => [
+                        'User not found'
+                    ]
+                ]
+            ];
+            return response()->json($data, 404);
+        }
+
+        return $user;
+    }
+
+    public static function checkToken(Request $request)
+    {
+        $token = $request->header('Token');
+
+        if(!$token){
+            $data = [
+                'success' => false,
+                'message' => 'The token not exist.'
+            ];
+            return response()->json($data, 401);
+        }
+
+        $token = ApiToken::query()->where('token', $token)->first();
+
+        $created = strtotime($token->created_at);
+        $end = $created + (40*60);
+        $now = strtotime(now());
+        $status = $now<$end;
+
+        if(!$status){
+            $data = [
+                'success' => false,
+                'message' => 'The token expired.'
+            ];
+            return response()->json($data, 401);
+        }
+//        Delete token
+        $token->delete();
+//        End of Validation token
+    }
+
+
+    public static function validateCreateUser($data)
+    {
+        $user = User::query()
+            ->where('email', $data['email'])
+            ->orWhere('phone', $data['phone'])
+            ->first();
+
+        if($user){
+            $data = [
+                'success' => false,
+                'message' => 'User with this phone or email already exist',
+            ];
+            return response()->json($data, 409);
+        }
+
+        if(!Position::query()->find($data['position_id'])){
+            $data = [
+                'success' => false,
+                'message' => 'Position with this id not exist',
+            ];
+            return response()->json($data, 404);
+        }
+    }
+}
