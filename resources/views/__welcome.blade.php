2ea849f9251f261fbd5666c7f8de2216d66f1762
Vova <vova.banudz@gmail.com>
2024-01-06 12:24:21 +0200

    frontend

--- resources/views/__welcome.blade.php
+++ resources/views/__welcome.blade.php
@@ -10,13 +10,45 @@
 </head>
 <body>
 
-<div class="d-flex justify-content-center mt-5">
+<div class="d-flex justify-content-center mt-5 flex-column" style="width: 50%; margin-right: auto; margin-left: auto">
 
 
 
 
-    <form action="{{route('send')}}" method="post" enctype="multipart/form-data">
-        @csrf
+    <div class="mt-5">
+        <div class="d-flex justify-content-center">
+            <button class="btn-list btn btn-primary btn-lg m-3">Show more</button>
+
+            <div class="mb-3 m-3">
+                <label class="form-label">Page</label>
+                <input type="number" name="page" class="form-control">
+            </div>
+
+            <div class="mb-3 m-3">
+                <label class="form-label">Count</label>
+                <input type="number" value="6" name="count" class="form-control">
+            </div>
+
+            <div class="mb-3 m-3">
+                <label class="form-label">Offset</label>
+                <input type="number" name="offset" class="form-control">
+            </div>
+        </div>
+        <div class="container mt-3">
+{{--            <div class="item border border-secondary p-3 mt-3">--}}
+{{--                <p>Name: <span>Rick</span></p>--}}
+{{--                <p>Phone: <span>+3898598695865</span></p>--}}
+{{--                <p>Email: <span>fgcfg@vgvg.cdsc</span></p>--}}
+{{--                <p>Position: <span>Docktor</span></p>--}}
+{{--                <img width="70px" height="70px" src="{{asset('storage/images/'). '/dchdvchjd.jpg'}}" alt="">--}}
+{{--            </div>--}}
+        </div>
+    </div>
+
+    <div class="mt-5">
+{{--    <form action="{{route('send')}}" method="post" enctype="multipart/form-data">--}}
+{{--    <form>--}}
+{{--        @csrf--}}
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Name</label>
             <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -39,54 +71,217 @@
             <label for="exampleInpudsdcs" class="form-label">Photo</label>
             <input type="file" name="photo" class="form-control" id="exampleInpudsdcs">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+            <button class="btn btn-primary btn-create">Submit</button>
+{{--    </form>--}}
 
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
 </div>
-
 
 <script>
 
+    let btnList = document.querySelector('.btn-list');
+    let container = document.querySelector('.container');
+    let page = document.querySelector('input[name="page"]');
+    let count = document.querySelector('input[name="count"]');
+    let offset = document.querySelector('input[name="offset"]');
+    // let url = 'http://localhost:8000/apiTest/public/api/v1/users'
+    let url = window.location.href + 'api/v1/users'
 
-    var formData = new FormData(); // file from input type='file'
-    var fileField = document.querySelector('input[type="file"]');
-    formData.append('position_id', 2);
-    formData.append('name', 'Jhon');
-    formData.append('email', 'Jhon@gmail.com');
-    formData.append('phone', '+380955388485');
-    formData.append('photo', fileField.files[0]);
+    btnList.addEventListener('click', function (){
+        container.innerHTML = ''
+        if(page.value || count.value || offset.value){
+            url += '?';
+        }
+        if(page.value != 0){
+            url += 'page=' + page.value + '&'
+        }
+        if(count.value != 0){
+            url += 'count=' + count.value + '&'
+        }
+        if(offset.value != 0){
+            url += 'offset=' + offset.value + '&'
+        }
 
-    fetch('http://localhost:8000/apiTest/public/api/v1/users',
-        { method: 'POST', body: formData, headers:
-                { 'Token': token,
-        // get token with GET api/v1/token method
-                },
-        })
-        .then(function(response)
-            { return response.json(); })
-        .then(function(data)
-            { console.log(data);
+        fetch(url)
+            .then(function(response) {
+
+                if(!response.ok){
+                    url = window.location.href + 'api/v1/users'
+                    page.value = 0
+                    count.value = 0
+                    offset.value = 0
+                    console.log(response.ok)
+                }
+                return response.json();
+            })
+            .then(function(data) {
+                data.users.forEach(item => {
+                    let listItem = document.createElement('div');
+                    listItem.classList.add('item')
+                    listItem.classList.add('border')
+                    listItem.classList.add('border-secondary')
+                    listItem.classList.add('p-3')
+                    listItem.classList.add('mt-3')
+
+                    let idP = document.createElement('p');
+                    let idSpan = document.createElement('span');
+                    idP.textContent = 'Id: ';
+                    idSpan.textContent = item.id;
+                    idP.appendChild(idSpan);
+                    listItem.appendChild(idP);
+
+                    let nameP = document.createElement('p');
+                    let nameSpan = document.createElement('span');
+                    nameP.textContent = 'Name: ';
+                    nameSpan.textContent = item.name;
+                    nameP.appendChild(nameSpan);
+                    listItem.appendChild(nameP);
+
+                    let phoneP = document.createElement('p');
+                    let phoneSpan = document.createElement('span');
+                    phoneP.textContent = 'Phone: ';
+                    phoneSpan.textContent = item.phone;
+                    phoneP.appendChild(phoneSpan);
+                    listItem.appendChild(phoneP);
+
+                    let emailP = document.createElement('p');
+                    let emailSpan = document.createElement('span');
+                    emailP.textContent = 'Email: ';
+                    emailSpan.textContent = item.email;
+                    emailP.appendChild(emailSpan);
+                    listItem.appendChild(emailP);
+
+                    let positionP = document.createElement('p');
+                    let positionSpan = document.createElement('span');
+                    positionP.textContent = 'Position: ';
+                    positionSpan.textContent = item.position;
+                    positionP.appendChild(positionSpan);
+                    listItem.appendChild(positionP);
+
+                    let photo = document.createElement('img');
+                    let src = item.photo
+                    if(item.photo.startsWith('images')){
+                        src = window.location.href + 'storage/' + item.photo;
+                    }
+                    photo.setAttribute('src', src);
+                    listItem.appendChild(photo);
+
+                    container.appendChild(listItem);
+
+                });
+
+                let prev = document.createElement('a');
+                prev.textContent = 'Prev';
+                prev.setAttribute('href', data.links.prev_url);
+
+                let next = document.createElement('a');
+                next.textContent = 'Next';
+                next.setAttribute('href', data.links.next_url);
+
+                let divLinks = document.createElement('div');
+
+                divLinks.appendChild(prev);
+                divLinks.appendChild(next);
+                container.appendChild(divLinks);
+
+                url = window.location.href + 'api/v1/users'
+                page.value = 0
+                count.value = 0
+                offset.value = 0
+
                 if(data.success)
                 {
-                 // process success response
-                } else {
-                 // proccess server errors
+                    // console.log('success')
+                }
+                else {
+                    // console.log('server errors')
                 }
             })
-        .catch(function(error)
-        {
-         //proccess network errors
-        });
+    })
+
+
+
+
+    let btnCreate = document.querySelector('.btn-create')
+    btnCreate.addEventListener('click', function (){
+        var formData = new FormData(); // file from input type='file'
+        var fileField = document.querySelector('input[type="file"]');
+        formData.append('position_id', document.querySelector('input[name="position_id"]').value);
+        formData.append('name', document.querySelector('input[name="name"]').value);
+        formData.append('email', document.querySelector('input[name="email"]').value);
+        formData.append('phone', document.querySelector('input[name="phone"]').value);
+        formData.append('photo', fileField.files[0]);
+
+
+        let token = ''
+        let urlToken = url = window.location.href + 'api/v1/token'
+
+            fetch(urlToken)
+                .then(function(response) {
+                    // console.log(response);
+
+                    return response.json();
+                })
+                .then(function(data)
+                {
+                    token = data.token
+                })
+                .catch(function(error)
+                {
+                    // proccess network errors
+                });
+
+        setTimeout(() => {
+
+            fetch('http://localhost:8000/apiTest/public/api/v1/users',
+                { method: 'POST', body: formData, headers:
+                        {'Token': token},
+                })
+                .then(function(response)
+                {return response.json();})
+                .then(function(data)
+                {
+                    if(data.success == false){
+
+
+                        let message = ''
+                        data.fails.forEach(item => {
+                            console.log(item);
+                            message += item
+                        })
+                        console.log(message);
+                        alert(message)
+                    }
+                    // console.log(data);
+                    if(data.success)
+                    {
+                        // alert(data.message)
+                        // process success response
+                    } else {
+                        // proccess server errors
+                    }
+                })
+                .catch(function(error)
+                {
+                    //proccess network errors
+                });
+
+        }, 2000);
+
+    })
+
+
+
 
 
 
